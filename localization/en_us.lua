2c0368f69710de635c5ae003693ca93df8074c70
Tinfoilbot65 <140971219+Tinfoilbot65@users.noreply.github.com>
2025-07-07 22:49:22 -0500

    Added CreditLib's Localization

--- localization/en_us.lua
+++ localization/en_us.lua
@@ -1,10 +1,15 @@
 return{
-   descriptions = {
-
-   },
-   misc = {
-	dictionary = {
-      ph_mafioso = "Saved by Mafioso"
-   },
-},
+   	descriptions = {
+   	},
+   	misc = {
+			dictionary = {
+     			 ph_mafioso = "Saved by Mafioso",
+			}
+			v_dictionary = {
+			a_art = { "Art: #1#" },
+			a_code = { "Code: #1#" },
+			a_idea = { "Idea: #1#" },
+			}
+   		},
+	},
 }
