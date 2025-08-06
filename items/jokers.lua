895650852dad63851e41db7997f6f6191a06d16e
Tinfoilbot65 <140971219+Tinfoilbot65@users.noreply.github.com>
2025-08-05 23:45:32 -0500

    Added Shigesato Itoi Joker

--- items/jokers.lua
+++ items/jokers.lua
@@ -1795,6 +1795,113 @@ SMODS.Joker {
         idea = { "Shan" }
     }
 }
+
+SMODS.Joker { -- Shigesato Itoi
+    name = "Shigesato Itoi",
+    key = "shigesatoitoi",
+    loc_txt = {
+        name = "Shigesato Itoi",
+        text = {
+            "If played hand is {C:attention}Two Pair{}",
+            "and the only suit is Hearts,",
+            "{C:red}destroy{} a random {C:attention}Joker{} if any exist",
+            "and adds varying {X:red,C:white}XMult{}",
+            "{C:inactive}(Currently{} {X:red,C:white}X#1#{}{C:inactive}){}",
+            "{C:inactive}I'm betting Jimbo solos Giygas. - Tin{}",
+        }
+    },
+    pos = { x = 0, y = 0 },
+    cost = 10,
+    rarity = 3,
+    blueprint_compat = true,
+    eternal_compat = true,
+    unlocked = true,
+    discovered = true,
+    pools = { ['SnipersTVAdditions'] = true },
+    config = {
+        extra = {
+            xmult = "1",
+            type = "Two Pair",
+            odds = "15",
+            xmultadd1 = "1982",
+            xmultadd2 = "1994",
+            xmultadd3 = "2006",
+        }
+    },
+
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.xmult } }
+    end,
+
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                Xmult = card.ability.extra.xmult
+            }
+        end
+        if context.cardarea == G.jokers and context.joker_main then
+            if (context.poker_hands[card.ability.extra.type] and (function()
+                    local allMatchSuit = true
+                    for i, c in ipairs(context.scoring_hand) do
+                        if not (c:is_suit("Hearts")) then
+                            allMatchSuit = false
+                            break
+                        end
+                    end
+
+                    return allMatchSuit and #context.scoring_hand > 0
+                end)()) then
+                if pseudorandom('shigesatomother3') < G.GAME.probabilities.normal / card.ability.extra.odds then
+                    card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmultadd3
+                    return {
+                        message = localize('k_upgrade_ex'),
+                        colour = G.C.MULT
+                    }
+                end
+                if pseudorandom('shigesatoearthbound') < G.GAME.probabilities.normal * 10 / card.ability.extra.odds then
+                    card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmultadd2
+                    return {
+                        message = localize('k_upgrade_ex'),
+                        colour = G.C.MULT
+                    }
+                end
+                if pseudorandom('shigesatomother1') < G.GAME.probabilities.normal * 5 / card.ability.extra.odds then
+                    card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmultadd1
+                    return {
+                        message = localize('k_upgrade_ex'),
+                        colour = G.C.MULT
+                    }
+                end
+                local destructable_jokers = {}
+                for i, joker in ipairs(G.jokers.cards) do
+                    if joker ~= card and not joker.ability.eternal and not joker.getting_sliced then
+                        table.insert(destructable_jokers, joker)
+                    end
+                end
+                local target_joker = #destructable_jokers > 0 and
+                    pseudorandom_element(destructable_jokers, pseudoseed('destroy_joker')) or nil
+
+                if target_joker then
+                    target_joker.getting_sliced = true
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            target_joker:start_dissolve({ G.C.RED }, nil, 1.6)
+                            return true
+                        end
+                    }))
+                    card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil,
+                        { message = "Destroyed!", colour = G.C.RED })
+                end
+            end
+        end
+    end,
+    credits = {
+        art = { "N/A" },
+        code = { "Tinfoilbot65" },
+        idea = { "Masked Man" },
+    }
+}
+
 SMODS.Rarity:take_ownership("Common", {
 	key = "Common",
 	loc_txt = {},
